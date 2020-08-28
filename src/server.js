2278b67ab2088b9e16c0e195e4d72384f66b9064
Breno <breno.novais@hotmail.com>
2020-08-28 20:19:24 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -1,21 +1,31 @@
-// Servidor
-const express = require('express');
-const server = express();
+const express = require('express')
+const server = express()
 
-const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+const  {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
 
-const nunjucks = require('nunjucks');
+
+//configurar nunjucks (template engine)
+const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
-  express: server,
-  noCache: true,
+    express: server,
+    noCache: true,
 })
 
+// Inicio e configuração do servidor
 server
+// receber os dados do req.body
 .use(express.urlencoded({ extended: true }))
+// configurar arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
+// rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
 .post("/save-classes", saveClasses)
-
-.listen(5500);
+// start do servidor
+.listen(5500)
